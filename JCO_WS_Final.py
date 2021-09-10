247655ec32ce49c4fba2268ba6918790352ee40e
Fito147 <87987851+Fito147@users.noreply.github.com>
2021-09-10 14:49:54 -0300

    commit

--- JCO_WS_Final.py
+++ JCO_WS_Final.py
@@ -6,7 +6,7 @@ import time
 
 url = 'https://ascopubs.org/loi/jco'
 time.sleep(3)
-page = requests.get(url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
+page = requests.get(url, headers={'User-Agent':'Your BrowserUser-Agent'})
 soup = BeautifulSoup(page.content, "html.parser")
 
 #%% Buscar que obtener de cada item (url,Issue,Fecha)
@@ -19,14 +19,13 @@ def extraer_info (links):
 
 raw_data = [extraer_info(links) for links in soup.find_all('div', class_="js_issue row")]
 df1 = pd.DataFrame (raw_data, columns=('Pagina Web', 'Issue', 'Fecha'))
-df1.to_csv('JCO_First_V2.0.csv', index=False)
+df1.to_csv('JCO_ALL_ISSUE.csv', index=False)
 
 issue_full_list = list (df1['Pagina Web'].astype(str).tolist())
 #%%
 #%%
 #%% Segunda Capa
 # Buscar que obtener de cada item (url, titulo y seccion de cada paper)
-# issue_url = "https://ascopubs.org/toc/jco/39/21"
 
 def extraer_info_paper (link_paper):
     doi = link_paper.find('a', class_="ref nowrap full")['href']
@@ -37,17 +36,14 @@ def extraer_info_paper (link_paper):
 def procesar_paginas(n): 
     issue_url = issue_full_list[n]
     time.sleep(3)
-    page = requests.get(issue_url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
+    page = requests.get(issue_url, headers={'User-Agent':'Your BrowserUser-Agent'})
     soup = BeautifulSoup(page.content, "html.parser")
     papers_data = [extraer_info_paper(link_paper) for link_paper in soup.find_all('table', class_="articleEntry")]
     return pd.DataFrame(papers_data, columns=('Titulos', 'DOI', 'URL'))
 #%% Scrapping en la cantidad de paginas que queremos
 
-df2 = pd.concat([procesar_paginas(n) for n in range(61,89)], ignore_index=True)
-df2.to_csv('JCO_2019_2_URLS.csv', index=False)
-
-# url_df = df2['URL']
-# url_df.to_csv('JCO_2019_1_URL_Only.csv', index=False)
+df2 = pd.concat([procesar_paginas(n) for n in range(0,10)], ignore_index=True)
+df2.to_csv('JCO_20XX_URLS.csv', index=False)
 
 pages = list (df2['URL'].astype(str).tolist())
 
@@ -64,7 +60,7 @@ section=list()
 
 for n in pages:
     time.sleep(3)
-    page = requests.get(n, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
+    page = requests.get(n, headers={'User-Agent':'Your BrowserUser-Agent'})
     soup = BeautifulSoup(page.content, "html.parser")
 
     #Title
@@ -84,16 +80,8 @@ for n in pages:
     volume.append(vol)
 
     df = pd.DataFrame({"Title": titles, "Authors": authors, "DOI": dois, "Section": section, "Vol-Issue": volume})
-    # df5 = df[['DOI', 'Title']]
-    # df6 = df[['DOI', 'Section']]
-    # df7 = df[['DOI', 'Vol-Issue']]
-    # df8 = df[['DOI', 'Authors']]
     
-    df.to_csv('JCO_2019_2.csv', index=False)
-    # df5.to_csv('JCO_2019_2_Titles.csv', index=False)
-    # df6.to_csv('JCO_2019_2_Section.csv', index=False)
-    # df7.to_csv('JCO_2019_2_Vol_Issue.csv', index=False)
-    # df8.to_csv('JCO_2019_2_Authors.csv', index=False)
+    df.to_csv('JCO_20XX.csv', index=False)
 
 
 #%%
